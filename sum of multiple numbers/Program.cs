a30714e2f1b0ea035abed18ae88a865e3bade6dc
Elioraroshni <tanzimarahman13@gmail.com>
2019-10-08 01:52:46 -0700

    Add project files.

---
+++ sum of multiple numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum_of_multiple_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i =1; i<1000; i++)
+            {
+                if ((i%3==0) || (i%5==0))
+                {
+                   //Console.WriteLine(i);
+                    sum = sum + i;
+                }
+                
+
+            }
+            Console.WriteLine("Sum of these multiples of 3 and 5 is");
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
